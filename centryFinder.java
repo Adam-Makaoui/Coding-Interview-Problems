c678d4a84cad855ee79703c0a77d7255aa901d10
Adam <31462418+Adam-Makaoui@users.noreply.github.com>
2020-12-25 23:56:02 -0800

    Add files via upload
    Added "centryFinder.java"

---
+++ centryFinder.java
@@ -0,0 +1,37 @@
+
+public class centryFinder {
+
+	static int test1 = 1905;
+	static int test2 = 95;
+	static int test3 = 2000;
+	
+	static int centuryFromYear(int year) {
+		
+	    // 1st centry
+	    if (year <= 100)
+	        return 1;
+	        
+	    // current centry e.g. 1900 = 19th centry
+	     else if (year % 100 == 0)
+	        return year/100;
+	        
+	    // next centry e.g. 1905 = 20th centry
+	    else
+	        return year/100 + 1;
+	}
+	
+	// Unit testing
+	public static void main(String[] args) {
+
+		// Unit testing - insert into function
+		System.out.print("Expect output: 20 \nActual output: ");
+		System.out.println(centuryFromYear(test1));
+		
+		System.out.print("\nExpect output: 1 \nActual output: ");
+		System.out.println(centuryFromYear(test2));
+		
+		System.out.print("\nExpect output: 20 \nActual output: ");
+		System.out.println(centuryFromYear(test3));
+	}
+
+}
