4c7bfe04ea245400af76d0592dede2dc5f3fc835
Adam <31462418+Adam-Makaoui@users.noreply.github.com>
2020-12-25 23:57:37 -0800

    Update centryFinder.java

--- centryFinder.java
+++ centryFinder.java
@@ -1,4 +1,11 @@
-
+/**
+ * Given a year, return the century it is in. The first century spans from the 
+ * year 1 up to and including the year 100, the second - from the year 101 up 
+ * to and including the year 200, etc.
+ * 
+ * @author Amak
+ *
+ */
 public class centryFinder {
 
 	static int test1 = 1905;
