4688c34d1a834c4c115ca88a9dca3bf7e1e17fa3
Adam Makaoui <31462418+Adam-Makaoui@users.noreply.github.com>
2021-06-28 12:35:51 -0400

    Add files via upload
    Removes duplicates in an array in O(1) space complexity.

---
+++ sortArrayOptimizedSpace.py
@@ -0,0 +1,27 @@
+def removeDuplicates(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+
+    # will tell us where to be in the array/list
+    pos = 0
+
+    # indexing using manual icrementation of pos
+    for _ in range(pos, len(nums)):
+
+        # check if array's value at index is a duplicated in the array
+        if nums.count(nums[pos]) >= 2:
+
+            # remove function at the current index (a duplicate), remove reorders arrays size and positioning
+            nums.remove(nums[pos])
+
+        # if we dont encounter a duplicate, increment index to check the next number
+        else:
+            pos += 1
+
+
+# driver code
+arr = [1, 2 ,2 ,3 ,3 ,3 ,2, 2, 5]
+removeDuplicates(arr)
+print(arr)
